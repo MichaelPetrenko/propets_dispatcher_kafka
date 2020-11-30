0b80a4fa06b2812a432c65c410a1965e0b34bc30
Severa3 <reizmp4@gmail.com>
2020-12-01 01:28:04 +0200

    Initial commit version

---
+++ ProPets_kafka_dispatcher/src/test/java/telran/propets/dispatcher/ProPetsKafkaDispatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package telran.propets.dispatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProPetsKafkaDispatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
