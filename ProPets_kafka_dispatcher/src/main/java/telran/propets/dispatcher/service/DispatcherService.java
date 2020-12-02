b5ca36f2128090112f96dbc467ebc70c92affbe8
Severa3 <reizmp4@gmail.com>
2020-12-03 00:29:54 +0200

    Pseudocode plan;

--- ProPets_kafka_dispatcher/src/main/java/telran/propets/dispatcher/service/DispatcherService.java
+++ ProPets_kafka_dispatcher/src/main/java/telran/propets/dispatcher/service/DispatcherService.java
@@ -12,6 +12,19 @@ public class DispatcherService {
 	@StreamListener(Sink.INPUT)
 	public void handlePost(LostFoundKafkaDto post) {
 		System.out.println(post.toString());
+		/**
+		 * Here we recieving post.
+		 * If (kafkaReqType==CREATE && typePost==false) => new post of lost pet => DB LostPets Elastic
+		 * If (kafkaReqType==CREATE && typePost==true) => new post of found pet => DB FoundPets Elastic
+		 * If (kafkaReqType==UPDATE && typePost==false) => update of lost pet => DB LostPets Elastic
+		 * If (kafkaReqType==UPDATE && typePost==true) => update of found pet => DB FoundPets Elastic
+		 * If (kafkaReqType==DELETE && typePost==false) => delete of lost pet => DB LostPets Elastic
+		 * If (kafkaReqType==DELETE && typePost==true) => delete of found pet => DB FoundPets Elastic
+		 * 
+		 *   TODO
+		 *   CRUD-methods in repo;
+		 *   Integration an ElasticTest project to here;
+		 */
 	}
 	
 }
