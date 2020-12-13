4fbfa3d46e1630af23e030826f5de3fb98ad6632
Severa3 <reizmp4@gmail.com>
2020-12-13 19:44:56 +0200

    Last stabile version before injection of logger

--- ProPets_kafka_dispatcher/src/main/java/telran/propets/dispatcher/ProPetsKafkaDispatcherApplication.java
+++ ProPets_kafka_dispatcher/src/main/java/telran/propets/dispatcher/ProPetsKafkaDispatcherApplication.java
@@ -2,7 +2,7 @@ package telran.propets.dispatcher;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//BeforeLogger
 @SpringBootApplication
 public class ProPetsKafkaDispatcherApplication {
 
