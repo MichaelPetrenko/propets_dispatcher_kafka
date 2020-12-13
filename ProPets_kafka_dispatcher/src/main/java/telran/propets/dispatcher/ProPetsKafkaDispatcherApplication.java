97eaacfd7274be045f220dca2441ec9af844cfed
Severa3 <reizmp4@gmail.com>
2020-12-13 21:05:46 +0200

    Trying to work with logger

--- ProPets_kafka_dispatcher/src/main/java/telran/propets/dispatcher/ProPetsKafkaDispatcherApplication.java
+++ ProPets_kafka_dispatcher/src/main/java/telran/propets/dispatcher/ProPetsKafkaDispatcherApplication.java
@@ -1,13 +1,22 @@
 package telran.propets.dispatcher;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//BeforeLogger
+
 @SpringBootApplication
 public class ProPetsKafkaDispatcherApplication {
+	
+	private static Logger logger = LogManager.getLogger("logspropets");
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProPetsKafkaDispatcherApplication.class, args);
+		
+        logger.debug("Debug log message");
+        logger.info("Info log message");
+        logger.error("Error log message");
+        
 	}
 
 }
