5dcd5232c4b1da2d69fa91db74599b7dabacec1b
Severa3 <reizmp4@gmail.com>
2020-12-17 20:46:50 +0200

    wakeUp

--- ProPets_kafka_dispatcher/src/main/java/telran/propets/dispatcher/controller/DispatcherController.java
+++ ProPets_kafka_dispatcher/src/main/java/telran/propets/dispatcher/controller/DispatcherController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DispatcherController {
 	
-	@GetMapping(value = "/dispatcher/wakeup")
+	@GetMapping(value = "/wakeup")
 	void wakeUp() {
 		return;
 	}
